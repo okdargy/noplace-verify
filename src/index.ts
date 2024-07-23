1b884ff17e61ca44bf1232bb431a8caae8a3732f
dargy <dragonismbot@protonmail.com>
2024-07-23 00:08:30 -0500

    feat: add status

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { Client } from 'seyfert';
+import { ActivityType, PresenceUpdateStatus } from 'seyfert/lib/types';
 
 const client = new Client();
-client.start().then(() => client.uploadCommands());
+
+client.start().then(async () => {
+    client.uploadCommands();
+
+    client.gateway.setPresence({
+        activities: [{
+            type: ActivityType.Playing,
+            name: "on noplace.app!",
+            state: " ",
+        }],
+        afk: false,
+        since: Date.now(),
+        status: PresenceUpdateStatus.Online,
+    })
+});
